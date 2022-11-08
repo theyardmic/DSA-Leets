f326f64d19a332e18839cac2b8d4a40581ecb91d
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-11-08 00:44:22 -0800

    Update Contains-Duplicate.js

--- Contains-Duplicate.js
+++ Contains-Duplicate.js
@@ -1,9 +1,7 @@
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
 let containsDuplicate = function(nums) {
     let uniqueArray = new Set(nums);
     return uniqueArray.size !== nums.length;
 };
+
